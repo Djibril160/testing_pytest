f3df504e38a59e47845be567cc9a4e68782df3d1
djibril soukouna <djibril.soukouna@deepki.com>
2026-02-26 09:58:10 +0100

    testing class base test

--- tests/tests_my_functions.py
+++ tests/tests_my_functions.py
@@ -4,3 +4,16 @@ import source.my_functions as my_functions
 def test_add():
     result = my_functions.add(1, 4)
     assert result == 5
+
+def test_add_strings():
+    result =  my_functions.add("I like ", "sleeping")
+    assert result  == "I like sleeping"
+
+def test_divide():
+    result = my_functions.divide(18, 9)
+    assert result == 2
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        my_functions.divide(2, 0)
+
